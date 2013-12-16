389b89473107602811acb2d069787e428e656518
Andrew Hao <ahao@blurb.com>
2013-12-15 17:43:35 -0800

    First Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HendrixApi::Application.config.secret_key_base = 'e91dde276f218651e437506153d84b9309aa9f83f8f411a03b1fbd3abe89469c30db90db9721a904ab15dbc044a49543523b93ed2e0a3eb4a2a1cbfd88d0bc96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HendrixApi::Application.config.session_store :cookie_store, key: '_hendrix-api_session'
