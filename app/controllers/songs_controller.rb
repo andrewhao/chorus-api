fc9d2586ee03458fcb218b68db7d835d9c2bcd56
Andrew Hao <andrewhao@gmail.com>
2013-12-26 05:28:37 -0800

    Heck yeah, passing song controller specs.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,40 @@
 class SongsController < ApplicationController
+  respond_to :json
 
+  def show
+    @song = find_song
+    respond_with @song
+  end
+  
+  def create
+    @song = Song.create song_params
+    respond_with @song
+  end
+
+  def index
+    @songs = Song.all
+    respond_with @songs
+  end
+
+  def destroy
+    @song = find_song
+    @song.destroy!
+    head :ok
+  end
+
+  def update
+    @song = find_song
+    @song.update! song_params
+    respond_with @song
+  end
+
+  private
+
+  def find_song
+    Song.find params[:id]
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :description, :key, :raw_text)
+  end
 end
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -5,7 +5,12 @@ describe SongsController do
   # This should return the minimal set of attributes required to create a valid
   # Song. As you add validations to Song, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { {  } }
+  let(:valid_attributes) { {
+    :title => "The Star Spangled Banner",
+    :description => "The iconic performance at Woodstock",
+    :key => "G",
+    :raw_text => "G A C\nOh say can you see"
+  } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -15,16 +20,26 @@ describe SongsController do
   describe "GET index" do
     it "assigns all songs as @songs" do
       song = Song.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {:format => "json"}, valid_session
       assigns(:songs).should eq([song])
     end
   end
 
   describe "GET show" do
+    let(:song) do
+      Song.create! valid_attributes
+    end
+
     it "assigns the requested song as @song" do
-      song = Song.create! valid_attributes
-      get :show, {:id => song.to_param}, valid_session
+      get :show, {:id => song.to_param, :format => "json"}, valid_session
       assigns(:song).should eq(song)
+    end
+
+    it "returns a song in JSON format" do
+      get :show, {:id => song.to_param, :format => 'json'}, valid_session
+
+      json = ActiveSupport::JSON.decode response.body
+      json.should be_kind_of Hash
     end
   end
 
@@ -32,19 +47,14 @@ describe SongsController do
     describe "with valid params" do
       it "creates a new Song" do
         expect {
-          post :create, {:song => valid_attributes}, valid_session
+          post :create, {:song => valid_attributes, :format => "json"}, valid_session
         }.to change(Song, :count).by(1)
       end
 
       it "assigns a newly created song as @song" do
-        post :create, {:song => valid_attributes}, valid_session
+        post :create, {:song => valid_attributes, :format => "json"}, valid_session
         assigns(:song).should be_a(Song)
         assigns(:song).should be_persisted
-      end
-
-      it "redirects to the created song" do
-        post :create, {:song => valid_attributes}, valid_session
-        response.should redirect_to(Song.last)
       end
     end
 
@@ -52,7 +62,7 @@ describe SongsController do
       it "assigns a newly created but unsaved song as @song" do
         # Trigger the behavior that occurs when invalid params are submitted
         Song.any_instance.stub(:save).and_return(false)
-        post :create, {:song => {  }}, valid_session
+        post :create, {:song => { :badparam => "hello" }, :format => "json"}, valid_session
         assigns(:song).should be_a_new(Song)
       end
     end
@@ -62,17 +72,15 @@ describe SongsController do
     describe "with valid params" do
       it "updates the requested song" do
         song = Song.create! valid_attributes
-        # Assuming there are no other songs in the database, this
-        # specifies that the Song created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Song.any_instance.should_receive(:update).with({ "these" => "params" })
-        put :update, {:id => song.to_param, :song => { "these" => "params" }}, valid_session
+
+        expect {
+          put :update, {:id => song.to_param, :song => { "title" => "params" }, :format => "json"}, valid_session
+        }.to change{ song.reload.title }.from(song.title).to("params")
       end
 
       it "assigns the requested song as @song" do
         song = Song.create! valid_attributes
-        put :update, {:id => song.to_param, :song => valid_attributes}, valid_session
+        put :update, {:id => song.to_param, :song => valid_attributes, :format => "json"}, valid_session
         assigns(:song).should eq(song)
       end
     end
@@ -82,18 +90,25 @@ describe SongsController do
         song = Song.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Song.any_instance.stub(:save).and_return(false)
-        put :update, {:id => song.to_param, :song => {  }}, valid_session
+        put :update, {:id => song.to_param, :song => { :badparam => "hello" }, :format => "json"}, valid_session
         assigns(:song).should eq(song)
       end
     end
   end
 
   describe "DELETE destroy" do
+    let(:song) { Song.create! valid_attributes }
+
     it "destroys the requested song" do
-      song = Song.create! valid_attributes
+      song
       expect {
-        delete :destroy, {:id => song.to_param}, valid_session
+        delete :destroy, {:id => song.to_param, :format => "json"}, valid_session
       }.to change(Song, :count).by(-1)
+    end
+
+    it "returns 200 OK" do
+      delete :destroy, {:id => song.to_param, :format => "json"}, valid_session
+      response.should be_ok
     end
   end
 end
