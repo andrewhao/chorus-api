60285e859d3d7acf179864dabe6686ed69340591
Andrew Hao <andrewhao@gmail.com>
2013-12-27 16:44:10 -0800

    Clean up CORS

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,10 @@ module HendrixApi
 
     config.middleware.use Rack::Cors do
       allow do
-        origins 'localhost:3000', '127.0.0.1:3000',
-        /http:\/\/localhost\/(:\d+)?/
+        #origins 'localhost:3000', '127.0.0.1:3000',
+        #/http:\/\/localhost\/(:\d+)?/
         # regular expressions can be used here
+        origins '*'
 
         resource '*',
           :headers => :any,
